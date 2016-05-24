0fe43709e0429716a4033cd00174d61a1ff570b1
Michael Loschiavo <losch103@gmail.com>
2016-05-23 23:09:04 -0700

    Added completed documents and edited ReadMe.md to include instructions for running and playing

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Tic_Tac_Toe
 Tic tac toe game programmed using Ruby
 
-This program is ran through the console.
+This program is ran through the console. Please use the command `ruby game.rb` to run the program.
+
+Place marks in a index-based format. For example "0,0" will place your mark in the upper left corner.
---
+++ board.rb
@@ -0,0 +1,65 @@
+# place_mark takes a position such as [0, 0] and a mark such as :X as arguments. It should throw an error if the position isn't empty.
+
+class Board
+  attr_reader :grid
+
+  def initialize(grid = [Array.new(3), Array.new(3), Array.new(3)])
+    @grid = grid
+  end
+
+  def place_mark(position, mark)
+    if position.first.is_a?(Array)
+      position.each do |el|
+        @grid[el[0]][el[1]] = mark
+      end
+    else
+      @grid[position[0]][position[1]] = mark
+    end
+  end
+
+  def empty?(position)
+    @grid[position[0]][position[1]].nil? ? true : false
+  end
+
+  def winner
+    horizontal_w = false
+    vertical_w = false
+    leftDiag_w = false
+    rightDiag_w = false
+    @grid.each do |row|
+      horizontal_w = :X if row.all? { |el| el == :X }
+      horizontal_w = :O if row.all? { |el| el == :O }
+    end
+    leftDiag_w = :X if (0..2).all? { |el| @grid[el][el] == :X }
+    leftDiag_w = :O if (0..2).all? { |el| @grid[el][el] == :O }
+
+    rightDiag_w = :X if (0..2).all? { |el| @grid[2 - el][el] == :X }
+    rightDiag_w = :O if (0..2).all? { |el| @grid[2 - el][el] == :O }
+
+    @grid.transpose.each do |row|
+      vertical_w = :X if row.all? { |el| el == :X }
+      vertical_w = :O if row.all? { |el| el == :O }
+    end
+
+    if !(horizontal_w || vertical_w || leftDiag_w || rightDiag_w)
+      nil
+    else
+      horizontal_w || vertical_w || leftDiag_w || rightDiag_w
+    end
+  end
+
+  def over?
+    if winner
+      return true
+    else
+      game_over = true
+      @grid.each do |row|
+        game_over = false if row.any?(&:nil?)
+      end
+    end
+    game_over
+  end
+
+
+
+end
